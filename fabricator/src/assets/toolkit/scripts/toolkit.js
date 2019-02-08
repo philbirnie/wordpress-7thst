be3561e26ab131be1ecb7a075645fc618fed86fe
Sean Sefcik <ssefcik@gmail.com>
2019-02-08 11:37:54 -0500

    Updates Fabricator to v 2.0.

--- fabricator/src/assets/toolkit/scripts/toolkit.js
+++ fabricator/src/assets/toolkit/scripts/toolkit.js
@@ -1,21 +1,3 @@
 /**
  * Toolkit JavaScript
  */
-require('es6-promise').polyfill();
-require('custom-event-polyfill'); // Adds CustomEvent support to IE
-require('picturefill');
-require('focus-visible'); // Polyfill for `:focus-visible`
-
-const $ = require('jquery');
-const boomsvgloader = require('boomsvgloader');
-const spriteSheetURL = document.body.getAttribute('data-sprites');
-
-// Load svg spritesheet
-if (spriteSheetURL) {
-    boomsvgloader.load(spriteSheetURL);
-}
-
-// Adds object-fit functionality to older browsers
-// const objectFitImages = require('object-fit-images');
-// const heroImg = document.querySelector('.c-hero__img img');
-// objectFitImages(heroImg);
