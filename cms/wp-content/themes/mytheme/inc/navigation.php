328312ed46db9b6d51de333a4cd575cd5ecebcd6
Phil Birnie <phil@7thstreetweb.com>
2019-09-17 07:42:19 -0400

    Updates Navigation Default so it is compliant; minor default style updates.

--- cms/wp-content/themes/mytheme/inc/navigation.php
+++ cms/wp-content/themes/mytheme/inc/navigation.php
@@ -1,15 +1,19 @@
 <?php
-/*
-|--------------------------------------------------------------------------
-| Navigation
-|--------------------------------------------------------------------------
-|
-|
-*/
+/**
+ * Navigation Registration and Global Functions
+ *
+ * @package    Noxgear
+ * @subpackage Noxgear
+ * @since      2019 Sep
+ */
 
-function register_navigation_menus() {
-	register_nav_menu( 'main', __( 'Main Navigation' ) );
-	register_nav_menu( 'utility', __( 'Utility Navigation' ) );
-	register_nav_menu( 'footer', __( 'Footer Navigation' ) );
+/**
+ * Register Navigation Menus
+ */
+function ssw_register_navigation_menus() {
+	register_nav_menu( 'main', __( 'Main Navigation', 'ssw' ) );
+	register_nav_menu( 'footer', __( 'Footer Navigation', 'ssw' ) );
+	register_nav_menu( 'heel', __( 'Heel Navigation', 'ssw' ) );
 }
-add_action( 'init', 'register_navigation_menus' );
+
+add_action( 'init', 'ssw_register_navigation_menus' );
